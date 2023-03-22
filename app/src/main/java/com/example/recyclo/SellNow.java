41a3bb163758e95e644fa5f2c3c7f1dae80eb6a3
Asus <divyanshsinghds2611@gmail.com>
2023-03-22 16:08:19 +0530

    sell ui/ux

---
+++ app/src/main/java/com/example/recyclo/SellNow.java
@@ -0,0 +1,14 @@
+package com.example.recyclo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellNow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_now);
+    }
+}
