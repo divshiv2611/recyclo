ff9edcc9ebe637ec264d2ad5eaeeb5c0b8ec7a7a
Asus <divyanshsinghds2611@gmail.com>
2023-03-22 17:13:51 +0530

    Last page UI

--- app/src/main/java/com/example/recyclo/MainActivity.java
+++ app/src/main/java/com/example/recyclo/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.recyclo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button move;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        move=findViewById(R.id.sell);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SellNow.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/recyclo/thankyoupage.java
@@ -0,0 +1,14 @@
+package com.example.recyclo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thankyoupage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thankyoupage);
+    }
+}
