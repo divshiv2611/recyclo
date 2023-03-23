0d2e6a0f99518d981b3c14598c3ca6de0a42f1b1
Asus <divyanshsinghds2611@gmail.com>
2023-03-23 15:03:58 +0530

    study now

--- app/src/main/java/com/example/recyclo/MainActivity.java
+++ app/src/main/java/com/example/recyclo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,12 +12,14 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button move;
+    private Button study;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         move=findViewById(R.id.sell);
+        study=findViewById(R.id.study);
         move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        study.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://avpn.asia/blog/the-opportunity-to-invest-in-strategies-that-transform-asias-waste-management-and-recycling-industry/");
+            }
+        });
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri=Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
